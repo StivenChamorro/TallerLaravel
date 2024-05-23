16b4a6faa7e95f59fd9f379e6f5f18054fcd701e
StivenChamorro <stivenchamorro493@gmail.com>
2024-05-23 10:14:44 -0500

    primer commit

---
+++ database/migrations/2024_05_23_014753_create_modules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modules', function (Blueprint $table) {
+            $table->id();
+            $table-> integer('code');
+            $table-> string('name');
+
+            $table->unsignedBigInteger('teacher_id')->unique();
+            $table->foreign('teacher_id')
+            -> references('id')
+            ->on('teachers')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modules');
+    }
+};
